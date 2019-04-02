597a036489a0d42ea07c3b7b784303d80b9e5284
ayse290799 <48259954+ayse290799@users.noreply.github.com>
2019-04-02 20:14:56 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,70 @@
+puan=0
+print("Türkiye'nin başkenti neresidir?")
+cevap=input("cevabı giriniz:")
+if cevap=="Ankara":
+    puan+=20
+    print("Tebrikler! Cevabınız doğru")
+    print("Şu anki puanınız:" +" "+str(puan))
+
+else:
+    print("cevabınız yanlış")
+    print("bu sorudan hiç puan alamadınız")
+    print("Şu anki puanınız: " +" "+str(puan))
+
+
+print("En küçük çift asal sayı nedir?")
+cevap=input("cevabı giriniz:")
+if cevap=="2":
+    puan+=20
+    print("Tebrikler! Cevabınız doğru")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+else:
+    print("cevabınız yanlış")
+    print("bu sorudan hiç puan alamadınız")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+
+print("Python dili sade ve açık bir dildir bilgisi doğru bir bilgi midir")
+cevap=input("cevabı giriniz:")
+if cevap=="Evet":
+    puan+=20
+    print("Tebrikler!Cevabınız doğru")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+else:
+    print("cevabınız yanlış")
+    print("bu sorudan hiç puan alamadınız")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+
+print("Serenad kitabının yazarı kimdir?")
+cevap=input("cevabı giriniz:")
+if cevap=="Zülfü Livaneli":
+    puan+=20
+    print("Tebrikler!Cevabınız doğru")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+else:
+    print("cevabınız yanlış")
+    print("bu sorudan hiç puan alamadınız")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+
+print("Hacettepe BBY 1972 yılında kurulmuştur bilgisi doğru bir bilgi midir?")
+cevap=input("cevabı giriniz:")
+if cevap=="Evet":
+    puan+=20
+    print("Tebrikler!Cevabınız doğru.")
+    print("Şu anki puanınız:"+" "+str(puan))
+
+else:
+    print("cevabınız yanlış")
+    print("bu sorudan hiç puan alamadınız")
+    print("şu anki puanınız:"+" "+str(puan))
+
+
+
+
+
+
